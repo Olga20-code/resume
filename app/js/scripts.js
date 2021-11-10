af1bb44b290a6ebc4ee70bc2e2e45499d4319f91
Olha Surikova <fordoing18@gmail.com>
2021-11-10 11:53:28 +0200

    Initial commit

---
+++ app/js/scripts.js
@@ -0,0 +1,71 @@
+'use strict';
+
+$(document).ready(function() {
+
+  // якір
+  $(".go-to").on('click',function(e){
+
+    e.preventDefault();
+
+    var anchor = $(this).attr("href");
+
+    if ($(anchor).length) {
+      var run = $(anchor).offset().top;
+      $('body,html').stop().animate({scrollTop: run}, 1500);
+    } else {
+      console.warn("ID don't search!")
+    }
+  });
+
+  $(".mobile-menu").on("click", function(){
+    $("header nav > ul").stop().slideToggle(function(){
+      if ($(this).css('display') === 'none'){
+        $(this).removeAttr('style');
+      }
+    });
+  });
+
+  $.fn.forceNumbericOnly = function () {
+    return this.each(function () {
+      $(this).keydown(function (e) {
+        var key = e.charCode || e.keyCode || 0;
+        return (key == 8 || key == 9 || key == 46 || (key >= 37 && key <= 40) || (key >=
+          48 && key <= 57) || (key >= 96 && key <= 105) || key == 107 || key ==
+          109 || key == 173 || key == 61);
+      });
+    });
+  };
+
+  $('input[type=tel]').forceNumbericOnly();
+
+  $('.list-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+
+  $('.dropdown').on("click", '.toggle', function (e) {
+    // $dropdownContainer.stop().slideToggle();
+    // $dropdownContainer.slideToggle();
+    let $this = $(this);
+    let $dropdownContainer = $this.parents('.dropdown').find('.dropdown-container');
+    $dropdownContainer.slideToggle();
+  });
+});
